4baf931f4d0d90630ad8f31f03f2edf40e661ca3
Dhruvil Prajapati <dhruvilprajapati2003@gmail.com>
2024-03-21 19:19:42 +0530

    added orign star

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,11 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      process.env.CORS_ORIGIN,
-      "http://localhost:5173",
-      "https://code-ide-x.web.app",
-    ],
+    origin: "*",
   })
 );
 
